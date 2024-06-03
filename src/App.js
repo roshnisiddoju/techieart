234f1c10a4308724c4b5bcd991f159187a97d701
roshnisiddoju <roshnisiddoju@gmail.com>
2024-06-03 17:55:02 +0530

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import mandala from './mandala.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={mandala} className="App-logo" alt="mandala" />
+        <p>
+          Welcome to my Arts Page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
